af95e837266139fd1e57ee7dbc69736eb15ea2f3
Paul Martin Panez Caballero <ppanezuni@gmail.com>
2024-10-05 16:58:17 -0500

    fix nasa service

--- services/nasa_service.py
+++ services/nasa_service.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 
 class NasaService():
@@ -7,12 +8,11 @@ class NasaService():
 
     def calculate_total_accumulated_precipitation(self, year, latitude, longitude):
         # Doc: https://power.larc.nasa.gov/api/pages/?urls.primaryName=Daily
-        url = f"https://power.larc.nasa.gov/api/projection/daily/point?start={year}0101& \
-                end={year}1231&latitude={latitude}&longitude={longitude}&community=ag&parameters=PRECTOTCORR"
+        url = f"https://power.larc.nasa.gov/api/projection/daily/point?start={year}0101&end={year}1231&latitude={latitude}&longitude={longitude}&community=ag&parameters=PRECTOTCORR"
         response = requests.get(url)
-        nasa_data = response.text
+        measurements_data = json.loads(response.text)["properties"]["parameter"]["PRECTOTCORR"]
         precipitation = 0.0
-        for measure in nasa_data["properties"]["parameter"]["PRECTOTCORR"]:
-            precipitation += measure[measure.keys()[0]]
+        for measure in measurements_data:
+            precipitation += measurements_data[measure]
         return precipitation // 365
 
