6181edf8f7abdd21119a7f3f5604f91ab8e052c1
Paul Martin Panez Caballero <ppanezuni@gmail.com>
2024-10-06 08:33:15 -0500

    considering temperature

--- models/crop.py
+++ models/crop.py
@@ -1,13 +1,15 @@
 class Crop():
-    def __init__(self, expected_performance, min_precipitation, max_precipitation, start_season, end_season):
+    def __init__(self, expected_performance, min_precipitation, max_precipitation, min_temperature, max_temperature, start_season, end_season):
         self.area = 0.0 # m2
         self.total_accumulated_precipitation = 0.0 # mm
-        self.expected_performance = expected_performance # Kg/m2
+        self.expected_performance = expected_performance # Kg/ha
         self.min_precipitation = min_precipitation
         self.max_precipitation = max_precipitation
+        self.min_temperature = min_temperature
+        self.max_temperature = max_temperature
         self.start_season = start_season
         self.end_season = end_season
-        # TODO: altitude, ground_type, temperature
+        # TODO: altitude, ground_type
     
     def calculate_area(self, coordinates):
         pass
--- services/nasa_service.py
+++ services/nasa_service.py
@@ -16,3 +16,23 @@ class NasaService():
             precipitation += measurements_data[measure]
         return precipitation // 365
 
+    def calculate_average_min_temperature(self, year, latitude, longitude):
+        # Doc: https://power.larc.nasa.gov/api/pages/?urls.primaryName=Daily
+        url = f"https://power.larc.nasa.gov/api/projection/daily/point?start={year}0101&end={year}1231&latitude={latitude}&longitude={longitude}&community=ag&parameters=T2M_MIN"
+        response = requests.get(url)
+        measurements_data = json.loads(response.text)["properties"]["parameter"]["T2M_MIN"]
+        min_temperature = 0.0
+        for measure in measurements_data:
+            min_temperature += measurements_data[measure]
+        return min_temperature // 365
+
+    def calculate_average_max_temperature(self, year, latitude, longitude):
+        # Doc: https://power.larc.nasa.gov/api/pages/?urls.primaryName=Daily
+        url = f"https://power.larc.nasa.gov/api/projection/daily/point?start={year}0101&end={year}1231&latitude={latitude}&longitude={longitude}&community=ag&parameters=T2M_MAX"
+        response = requests.get(url)
+        measurements_data = json.loads(response.text)["properties"]["parameter"]["T2M_MAX"]
+        max_temperature = 0.0
+        for measure in measurements_data:
+            max_temperature += measurements_data[measure]
+        return max_temperature // 365
+
